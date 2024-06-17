23cfe343f1be9538b0cae5d32ef217ef01075987
Maks Arefev <czenturijon@gmail.com>
2024-06-17 17:25:13 +0300

    fix: urls

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const http = require('http')
-const { randomUUID } = require('crypto')
+const http = require('http');
+const { randomUUID } = require('crypto');
 
-let reqCounter = 0
+let reqCounter = 0;
 
 const server = http.createServer( (req, res) => {
-
-  reqCounter++
-
-  switch (req.url) {
-    case 'requests':
-      res.write('Requests ' + reqCounter)
-      break
-
-    case 'auth':
-      res.write('UUID: ' + randomUUID())
-      break
-
-    default:
-      res.write('Not Found')
+  if (req.url !== '/favicon.ico') {
+    reqCounter++
   }
 
-  res.end()
+  switch (req.url) {
+    case '/requests':
+      res.write('Requests ' + reqCounter);
+      break;
+
+    case '/auth':
+      res.write('UUID: ' + randomUUID());
+      break;
+
+    default:
+      res.write('Not Found');
+  }
+
+  res.write(' => ' + reqCounter);
+  res.end();
 } )
 
-server.listen(3003)
+server.listen(3003);
