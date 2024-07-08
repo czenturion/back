653da8681d006274893be91c24d80620ddf134ac
Maks Arefev <czenturijon@gmail.com>
2024-07-08 20:57:32 +0300

    fix: home file path

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -9,6 +9,6 @@ getHomeRoutes.get('/', (req,
                         res) => {
   res
     .status(HTTP_STATUSES.OK_200)
-    .sendFile(path.join(__dirname, '..', 'pages', 'home.html'))
+    .sendFile(path.join(__dirname, '..', '..', 'pages', 'home.html'))
 })
 
